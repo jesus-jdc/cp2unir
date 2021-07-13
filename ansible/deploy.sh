10e3a2829c77ad99196ad4c5ae458a234af0ee56
jesus-jdc <74616281+jesus-jdc@users.noreply.github.com>
2021-07-13 13:37:35 +0200

    add: deploy nginx.yml - multiple fixes

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -9,3 +9,4 @@ ansible-playbook -i hosts install_cni.yml
 ansible-playbook -i hosts kubectl_config.yml
 ansible-playbook -i hosts sdn_azure.yml
 ansible-playbook -i hosts k8s_workers_config.yml
+ansible-playbook -i hosts ingress_controller.yml
